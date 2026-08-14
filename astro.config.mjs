f1d04fc79361d4d9516c0b92e85153074ca132a4
Dovid Levine <david@axepress.dev>
2026-08-14 18:06:36 +0300

    ci: ephemerial environments, aliases, and regression tests (#7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   site: 'https://wcus-ai.github.io',
+  vite: {
+    resolve: {
+      alias: {
+        '@lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@content': fileURLToPath(new URL('./src/content', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+        '@scripts': fileURLToPath(new URL('./src/scripts', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@init': fileURLToPath(new URL('./scripts', import.meta.url)),
+      },
+    },
+  },
 });
